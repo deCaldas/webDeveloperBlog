aa5d23d90092f743baf355e8fb40e6a220269214
Diego Toro <d.caldascaridad@gmail.com>
2023-11-04 13:45:53 -0500

    Built

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import Link from './Link'
 import siteMetadata from '@/data/siteMetadata'
 import SocialIcon from '@/components/social-icons'
-import Logo from '@/data/logo.svg'
+/* import Logo from '@/data/logo.svg' */
 
 export default function Footer() {
   return (
@@ -15,26 +15,22 @@ export default function Footer() {
         </div>
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>
-            <Link href="https://deCaldas.github.io/home/">
-            {siteMetadata.author}
-            </Link>
+            <Link href="https://deCaldas.github.io/home/">{siteMetadata.author}</Link>
           </div>
           <div>{` • `}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          Tailwind Nextjs Theme
-        </div>
-{/* 
+        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">Tailwind Nextjs Theme</div>
+        {/* 
           <Logo />
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+          <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
           <Link href="https://github.com/timlrx/tailwind-nextjs-starter-blog">
             Tailwind Nextjs Theme
           </Link>
-        </div>
-*/}
+          </div>
+        */}
       </div>
     </footer>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-/** 
- * *
- * import Logo from '@/data/logo.svg'
- * * 
- * **/
+/**  * import Logo from '@/data/logo.svg'  * **/
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
@@ -16,22 +12,19 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-
-          {/* ** Quisiera agregar mi propio Imagotipo ** */}
-{/* 
+            {/* ** Quisiera agregar mi propio Imagotipo ** */}
+            {/* 
             <div className="mr-3">
               <Logo />
             </div> 
-*/}
-
+            */}
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
               </div>
             ) : (
               siteMetadata.headerTitle
-            )} 
-
+            )}
           </div>
         </Link>
       </div>
